573b4edb03db291673bdf09160eb25d581598e3e
Doug Mill <doug@telmate.com>
2015-08-10 00:28:16 -0700

    root to threads index for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "threads#index"
+
   match "/401" => "default_errors#unauthenticated", via: [:get, :post, :put, :delete, :patch]
   match "/404" => "default_errors#not_found", via: [:get, :post, :put, :delete, :patch]
   match "/400" => "default_errors#bad_format", via: [:get, :post, :put, :delete, :patch]
