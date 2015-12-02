362ec843ddeed933ae5719d0269354f3abc9f2eb
Doug Mill <doug@telmate.com>
2015-12-01 22:03:23 -0800

    we don't need a browser pipeline

--- web/router.ex
+++ web/router.ex
@@ -2,14 +2,6 @@ defmodule EmpiriApi.Router do
   use EmpiriApi.Web, :router
   require Logger
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
     plug Joken.Plug, on_verifying: &EmpiriApi.Router.validate_auth_token/0,
