ef9117de79236884e355d54fb5930cda16ed5d47
Doug Mill <doug@telmate.com>
2015-12-01 23:04:03 -0800

    fix error view tests

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -4,18 +4,18 @@ defmodule EmpiriApi.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(EmpiriApi.ErrorView, "404.html", []) ==
-           "Page not found"
+  test "renders 404.json" do
+    assert render_to_string(EmpiriApi.ErrorView, "404.json", []) ==
+         to_string Poison.Encoder.encode(%{error: "Not Found"}, [])
   end
 
   test "render 500.html" do
-    assert render_to_string(EmpiriApi.ErrorView, "500.html", []) ==
-           "Server internal error"
+    assert render_to_string(EmpiriApi.ErrorView, "500.json", []) ==
+          to_string Poison.Encoder.encode(%{error: "Internal Server Error"}, [])
   end
 
   test "render any other" do
-    assert render_to_string(EmpiriApi.ErrorView, "505.html", []) ==
-           "Server internal error"
+    assert render_to_string(EmpiriApi.ErrorView, "505.json", []) ==
+          to_string Poison.Encoder.encode(%{error: "Internal Server Error"}, [])
   end
 end
