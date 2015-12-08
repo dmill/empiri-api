dc800e022d2184f1543a9c7159f22b1c4558712b
Doug Mill <doug@telmate.com>
2015-12-08 15:29:49 -0800

    don't define parent module explicitly

--- web/helpers/repo_extensions.ex
+++ web/helpers/repo_extensions.ex
@@ -1,32 +1,29 @@
-defmodule EmpiriApi.Helpers do
-  defmodule RepoExtensions do
+defmodule EmpiriApi.Helpers.RepoExtensions do
+  alias EmpiriApi.Repo
 
-    alias EmpiriApi.Repo
+  @doc """
+  Looks for a record in the repo for a given model. If a record is found it will return:
 
-    @doc """
-    Looks for a record in the repo for a given model. If a record is found it will return:
+  {:ok, model}
 
-    {:ok, model}
+  If no record is found, it will generate a changeset for the model and attempt to insert
+  with the given attrs. If the changeset is valid, it will return:
 
-    If no record is found, it will generate a changeset for the model and attempt to insert
-    with the given attrs. If the changeset is valid, it will return:
+  {:ok, model}
 
-    {:ok, model}
+  If the changeset is invalid, it will return:
 
-    If the changeset is invalid, it will return:
+  {:ok, model}
+  """
+  @spec get_or_insert_by(Ecto.Model.t, Map.t, Map.t) ::
+    {:ok, Ecto.Model.t} | {:error, Ecto.Changeset.t}
+  def get_or_insert_by(model, clauses, attrs \\ %{}) do
+    record = Repo.get_by(model, clauses)
 
-    {:ok, model}
-    """
-    @spec get_or_insert_by(Ecto.Model.t, Map.t, Map.t) ::
-      {:ok, Ecto.Model.t} | {:error, Ecto.Changeset.t}
-    def get_or_insert_by(model, clauses, attrs \\ %{}) do
-      record = Repo.get_by(model, clauses)
-
-      if record do
-        {:ok, record}
-      else
-        Repo.insert(model.changeset(struct(model), attrs))
-      end
+    if record do
+      {:ok, record}
+    else
+      Repo.insert(model.changeset(struct(model), attrs))
     end
   end
 end
