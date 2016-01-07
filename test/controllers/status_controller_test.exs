d8637a0bbb88f164c46fd16c93efb1c039ee781a
Doug Mill <doug@telmate.com>
2016-01-06 18:22:49 -0800

    remove these tests - no longer validating content-type on GETs

--- test/controllers/status_controller_test.exs
+++ test/controllers/status_controller_test.exs
@@ -1,18 +1,6 @@
 defmodule EmpiriApi.StatusControllerTest do
   use EmpiriApi.ConnCase
 
-  test "with no content-type header" do
-    conn = get conn, status_path(conn, :index)
-    assert json_response(conn, 415)["error"] == "Unsupported Media Type"
-  end
-
-  test "with an incorrect content-type header" do
-    connection = conn |> put_req_header("authorization", "Bearer bullshit") |> put_req_header("content-type", "multipart/form-data")
-    conn = get connection, status_path(connection, :index)
-
-    assert json_response(conn, 415)["error"] == "Unsupported Media Type"
-  end
-
   test "without an Authorization header" do
     connection = conn |> put_req_header("content-type", "application/json")
     conn = get connection, status_path(connection, :index)
