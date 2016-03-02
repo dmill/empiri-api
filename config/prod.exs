03622609a4b317946009ccfae3b130c21de87f9a
Doug Mill <doug@empiri.co>
2016-03-02 17:26:54 -0300

    another prod.exs change for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :empiri_api, EmpiriApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "fierce-inlet-12881.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "desolate-thicket-91367.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
@@ -23,7 +24,6 @@ config :logger, level: :info
 config :empiri_api, EmpiriApi.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  database: "empiri_prod",
   pool_size: 20
 
 # ## SSL Support
