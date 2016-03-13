d6667ae1b6c84532e07c769b4e470545355a8a6c
Doug Mill <doug@empiri.co>
2016-03-13 20:12:57 -0300

    remove bad old conflicting migration

--- priv/repo/migrations/20160205022451_change_publication_title_to_text.exs
+++
@@ -1,9 +0,0 @@
-defmodule EmpiriApi.Repo.Migrations.ChangePublicationTitleToText do
-  use Ecto.Migration
-
-  def change do
-    alter table(:hypotheses) do
-      modify :title, :text
-    end
-  end
-end
